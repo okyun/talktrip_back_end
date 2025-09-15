ef7647bde701bdc53c17abe5f7d4a46ec8eab0d6
okyun <dhrdbs8066@naver.com>
2025-09-15 21:37:47 +0900

    main 함수에 시간 함수 추가

--- src/main/java/com/talktrip/talktrip/TalkTripApplication.java
+++ src/main/java/com/talktrip/talktrip/TalkTripApplication.java
@@ -1,13 +1,23 @@
 package com.talktrip.talktrip;
 
+import com.talktrip.talktrip.global.config.SeoulDateTimeProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(dateTimeProviderRef = "seoulDateTimeProvider")
 public class TalkTripApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TalkTripApplication.class, args);
+    }
+
+    @Bean
+    public DateTimeProvider seoulDateTimeProvider() {
+        return new SeoulDateTimeProvider();
     }
 
 }
